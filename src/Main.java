abb2b87c97701747e2ac566aaa0163cffaed543f
dc0d3m3 <162016794+dc0d3m3@users.noreply.github.com>
2024-06-02 09:34:20 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import geometry.Point;
-import geometry.Circle;
+import geometry.ColoredCircle;
 
 public class Main {
     public static void main(String[] args) {
-        Point center = new Point(5.0, 5.0);
-        Circle circle = new Circle(center, 10.0);
-        System.out.println("Obwód koła: " + circle.calculatePerimeter());
-        System.out.println("Pole koła: " + circle.getArea());
+        Point center = new Point(10.0, 10.0);
+        ColoredCircle coloredCircle = new ColoredCircle(center, 15.0, "Red");
+        System.out.println("Kolor koła: " + coloredCircle.getColor());
+        System.out.println("Pole kolorowego koła: " + coloredCircle.getArea());
     }
 }
