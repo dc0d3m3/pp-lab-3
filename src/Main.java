2a9310b885f1bc2b1cd21afd959ae1b01ae3751d
dc0d3m3 <162016794+dc0d3m3@users.noreply.github.com>
2024-06-02 09:39:08 +0200

    zad5

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,9 @@
-import geometry.Circle;
-import geometry.ColoredCircle;
-import geometry.Point;
+import geometry.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        Circle[] circles = new Circle[] {
-                new Circle(new Point(1.0, 1.0), 2.0),
-                new ColoredCircle(new Point(2.0, 2.0), 3.0, "Blue"),
-                new Circle(new Point(3.0, 3.0), 4.0)
-        };
-
-        for (Circle circle : circles) {
-            System.out.println("Pole koła: " + circle.getArea());
-            if (circle instanceof ColoredCircle) {
-                System.out.println("Kolor: " + ((ColoredCircle)circle).getColor());
-            }
-        }
+        Rectangle rectangle = new Rectangle(5.0, 10.0);
+        System.out.println("Pole prostokąta: " + rectangle.calculateArea());
+        System.out.println("Obwód prostokąta: " + rectangle.calculatePerimeter());
     }
 }
